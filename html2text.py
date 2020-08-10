5871e20a85e65418e837de83b1e489553b5fda55
GaoBus2020 <60093496+GaoBus2020@users.noreply.github.com>
2020-08-10 09:38:45 +0800

    Add files via upload

---
+++ html2text.py
@@ -0,0 +1,24 @@
+import re
+from bs4 import BeautifulSoup
+from lxml import etree
+import  urllib
+ 
+html = '<p>你好</p><br/><font>哈哈</font><b>大家好</b>'
+ 
+# 法一
+pattern = re.compile(r'<[^>]+>',re.S)
+result = pattern.sub('', html)
+print(result)
+# 法二
+soup = BeautifulSoup(html,'html.parser')
+print(soup.get_text())
+ 
+# 法三
+response = etree.HTML(text=html)
+# print(dir(response))
+print(response.xpath('string(.)'))
+ 
+ 
+# 你好哈哈大家好
+# 你好哈哈大家好
+# 你好哈哈大家好
